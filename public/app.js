4b82107d51db1fb3cfbf3b152a97f5d26c93e5d5
Smoreh <108205817+8Bit-CCieCollab-Smore@users.noreply.github.com>
2025-09-17 08:35:58 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://deblocked-chat-production.up.railway.app/";
+const API_URL = "https://deblocked-chat-production.up.railway.app";
 
 let username = localStorage.getItem("username") || null;
 let currentRoom = "global";
